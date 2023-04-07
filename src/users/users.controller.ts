325f7e46a784318f0320e3f38a1e40530f7395aa
ramzimah <ramzimahnoune@gmail.com>
2023-04-07 04:14:18 +0100

    add the user service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(public usersService: UsersService) {}
+  @Post('/auth')
+  createUser(@Body() createUserDto: createUserDto) {
+    this.usersService.create(createUserDto.email, createUserDto.password);
+  }
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const userExists = await this.usersService.findOne(parseInt(id));
+    if (!userExists) {
+      throw new NotFoundException('user not found');
+    }
+    return userExists;
+  }
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), UpdateUserDto);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+  async findOne(id: number) {
+    return this.repo.findOneBy({ id: id });
+  }
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+  async update(id: number, data: Partial<User>) {
+    const userExists = await this.repo.findOneBy({ id });
+    if (!userExists) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(userExists, data);
+    return this.repo.save(userExists);
+  }
+  async remove(id: number) {
+    const userExists = await this.repo.findOneBy({ id });
+    if (!userExists) {
+      throw new NotFoundException('user not found');
+    }
+    return this.repo.remove(userExists);
+  }
+}
