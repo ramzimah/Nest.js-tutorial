fde3582c151c926920c912e27167f905bcfce54d
ramzimah <ramzimahnoune@gmail.com>
2023-06-28 15:54:32 +0100

    feature: add AdminGuard to prevent non admins from approving a report, add getEstimate endpoint

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -14,6 +16,8 @@ import { User } from 'src/users/users.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -29,11 +33,16 @@ export class ReportsController {
   }
 
   @Patch('/:id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
   async approveReport(
     @Param('id') id: string,
     @Body() approveReportDto: ApproveReportDto,
   ) {
     return this.reportService.changeApproval(id, approveReportDto.approved);
+  }
+  @Get()
+  @UseGuards(AdminGuard)
+  async getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportService.getEstimate(query);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from 'src/users/users.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -26,5 +27,27 @@ export class ReportsService {
     }
     reportExists.approved = approved;
     return this.repo.save(reportExists);
+  }
+  async getEstimate({
+    make,
+    model,
+    longitude,
+    latitude,
+    year,
+    mileage,
+  }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make=:make', { make })
+      .andWhere('model=:model', { model })
+      .andWhere('longitude-:longitude BETWEEN -5 AND 5', { longitude })
+      .andWhere('latitude-:latitude BETWEEN -5 AND 5', { latitude })
+      .andWhere('year-:year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage-:mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
